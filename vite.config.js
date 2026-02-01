0175c51eb3389a65e55fe59bf71ad6a7bd2ed75a
JK Coding HUB <jitkarmakar001@gmail.com>
2026-02-01 16:19:38 +0530

    Fix allowedHosts for Render

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    preview: {
+    server: {
     allowedHosts: "https://weather-app-dwh4.onrender.com",
   },
 })
