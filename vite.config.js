44c443cfe6876ab32ac8f7e8f8acf4f471758cbd
JK Coding HUB <jitkarmakar001@gmail.com>
2026-02-01 13:00:29 +0530

    fix vite allowedHosts for render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    allowedHosts: ["weather-app-react-ls9s.onrender.com"],
+  },
 })
